9e6050b57d5c36a52def3641c80491d6832c579e
Sathanand Uppuluri <sathanandu2002@gmail.com>
2025-04-25 10:19:37 +0530

    dental checkup application

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000'
+  baseURL: 'http://localhost:5000' || 'https://dental-checkup-system-w5gt.onrender.com'
 });
 
 api.interceptors.request.use(
